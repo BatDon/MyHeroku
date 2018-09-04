069181e0ef272e8dac231c4543b96619036cc0e1
BatDon <asics007@gmail.com>
2018-09-04 18:37:01 -0500

    First commit

--- app/src/main/java/com/example/david/myheroku/MainActivity.java
+++ app/src/main/java/com/example/david/myheroku/MainActivity.java
@@ -5,24 +5,46 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 //ratpack
+import ratpack.exec.Blocking;
+import ratpack.hikari.HikariModule;
+import ratpack.server.BaseDir;
+import ratpack.server.RatpackServer;
+import ratpack.groovy.template.TextTemplateModule;
+import ratpack.guice.Guice;
 import ratpack.test.embed.EmbeddedApp;
-import static org.junit.Assert.assertEquals;
+//import static org.junit.Assert.assertEquals;
+import static ratpack.groovy.Groovy.groovyTemplate;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.*;
+import java.sql.*;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+import javax.sql.DataSource;
 
-        final Button button = findViewById(R.id.heroku_button);
-        button.setOnClickListener(new View.OnClickListener() {
-                                      public void onClick(View v) {
+//retrofit
+
+package my.app;
+
+import ratpack.server.RatpackServer;
+
+public class Main {
+    public static void main(String... args) throws Exception {
+        RatpackServer.start(server -> server
+                .handlers(chain -> chain
+                        .get(ctx -> ctx.render("Hello World!"))
+                        .get(":name", ctx -> ctx.render("Hello " + ctx.getPathTokens().get("name") + "!"))
+                )
+        );
+    }
+}
+
+//        final Button button = findViewById(R.id.heroku_button);
+//        button.setOnClickListener(new View.OnClickListener() {
+//                                      public void onClick(View v) {
 
 
                                           // Code here executes on main thread after user presses button
                                       }
-    }
+                                      )}
 
     //Button herokuButton = (Button) findViewById(R.id.herokuButton);
-}
+        )}
